6672498d7127096cdcf824ecde68ab0efaa8490e
matthew.serre <matthew.serre@gmail.com>
2018-10-31 00:32:14 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  return nil if name_hash == {}
+  lowest_key_value = 0
+  key_name = []
+  name_hash.each { |key,value|
+    if lowest_key_value > value
+      lowest_key_value = value
+      key_name[0] = key
+    end
+  }
+  key_name[0]
 end
