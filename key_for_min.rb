42130f6a131aae24df44faf0434a9ed0ea805c37
matthew.serre <matthew.serre@gmail.com>
2018-10-31 01:15:11 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -26,7 +26,7 @@ def key_for_min_value(name_hash)
   comparison_value = name_hash.first[1]
   key_name = key_name
   name_hash.each do |key,value|
-    if value < comparison_value
+    if value <= comparison_value
       comparison_value = value
       key_name = key
     end
